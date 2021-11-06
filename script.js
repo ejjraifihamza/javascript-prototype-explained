75f4a33b4254332a49328c91e6167b1382e9e1dc
ejjraifihamza <ejjraifihamza@gmail.com>
2021-11-06 18:35:01 +0100

    🚀 Add and Access to Methods and Properties

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
-/*
-! Prototype property
-* function are object
-* function contains:
-    ? Methods
-    ? Properties
-    ? 'prototypes' Accessible
-* Initial value 'prototype' empty object
-* Augment prototype
-*/
+// ! create a function
+// function player() {
+//   this._fullName = "Jhon Doe"; // ! this keyword refer to object so value of function now are accessible from constructor
+// }
+// const player_1 = new player();
+// console.log(player_1._fullName); // * Jhon Doe
 
-// ! Function are object
-function player() {}
-console.log(typeof player); // function
-// but in this function you can access to methods and property just like object
-// ? for example some methods you cann access with this function
-player.call();
-player.apply();
-// ? for example some property you cann access with this methods
-console.log(player.length); // give how many arg in function
-// ? when you create a function you can access prototype and make it object
-console.log(typeof player.prototype); // object
-// * Initial value 'prototype' empty object its like you create a new object (player.prototype = {}) even if the function is contain some code
+// ! the elements of the function are now accessible from an constructor by this keyword
+function player(fullName, age) {
+  (this._fullName = fullName),
+    (this._age = age),
+    (this._getInfo = function () {
+      return `the player name is: ${this._fullName} and he's ${this._age} years old`;
+    });
+}
 
-// * Augment prototype (add some properties and methods)
 player.prototype = {
-  firstName: "Jhon",
-  lastName: "Doe",
-  fullName: function () {
-    return `${this.firstName} ${this.lastName}`;
+  _price: 2000,
+  _contry: "USA",
+  _getAllInfo: function () {
+    return `${this._getInfo()} and he coust ${this._price} $ and he is from ${
+      this._contry
+    }`;
   },
 };
-console.log(player.prototype.fullName()); // Jhon Doe
 
-/* 
-! Info : function player and player.prototype are separete (you can't access function from object, 
-! and the opposite is true)
-! No effection on the function
-*/
-// * you can access the elements of prototype if you create an constructor
-const player_1 = new player();
-console.log(player_1.fullName()); // Jhon Doe
+const player_1 = new player("Jhon Doe", 26);
+console.log(player_1._getAllInfo());
