719334b70e2417a33fa6f7da3b4cbccdb1bea9b9
ejjraifihamza <ejjraifihamza@gmail.com>
2021-11-07 17:26:53 +0100

    🚀 Add Custom Method to String Constructor

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
-// ! create an object 'engine'
-const engine = {
-  type: "Blink",
-  license: "GNU LGPL",
-  status: "Active",
+// ! check function, string, array, object methodes
+console.dir(String.prototype);
+console.dir(Function.prototype);
+console.dir(Array.prototype);
+console.dir(Object.prototype);
+
+// ! create a built-in method for the String constructor
+// * purpose of this built-in function is making the first character Uppercase
+String.prototype.capitalize = function () {
+  return `${this[0].toUpperCase()}${this.substr(1).toLowerCase()}`;
 };
 
-// ! create a constructor 'Browser'
-function Browser() {}
-
-// ! Reference an object 'engine' to the constructor 'Browser'
-Browser.prototype = engine;
-
-// ! Create a new instance of the 'Browser' constructor
-const browser = new Browser();
-
-// ! Give the selected propreties different values
-browser.type = "Gecko";
-browser.license = "Mozilla license";
-
-// ? Access to these properties
-console.log(browser.type); // Gecko
-console.log(browser.license); // Mozilla license
-console.log(browser); // {type: 'Gecko', license: 'Mozilla license'}
-
-// ? Look for a property that doesn't exist in the 'browser' object
-console.log(browser.status); // Active
-
-// ? Access to the secret link that acces to the 'prototype' object
-console.log(browser.__proto__); // {type: 'Blink', license: 'GNU LGPL', status: 'Active'}
-
-// ? check if the object 'engine' referenced to the 'Browser.prototype'
-// ? is the same as the object showed in 'browser.__proto__'
-console.log(browser.__proto__ === engine); // true
+let lastName = "jhon";
+lastName = lastName.capitalize();
+console.log(lastName);
